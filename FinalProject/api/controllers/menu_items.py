cc35098ed82bb3429cdfab73acacdf235eff432a
jaydenrrr22 <sryu@charlotte.edu>
2025-11-07 19:59:41 -0500

    added controllers for menu_items and resources

---
+++ FinalProject/api/controllers/menu_items.py
@@ -0,0 +1,73 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response
+from ..models import menu_item as model
+from ..schemas.menu_item import MenuItemCreation, MenuItemUpdate
+from sqlalchemy.exc import SQLAlchemyError
+
+from ..schemas.menu_items import MenuItemsUpdate
+
+
+def create(db: Session, request: MenuItemsCreation):
+    new_menu_item = model.MenuItem(
+        name=request.name,
+        description=request.description,
+        price=request.price,
+        quantity=request.quantity,
+        calories=request.calories,
+        category=request.category
+    )
+    try:
+        db.add(new_menu_item)
+        db.commit()
+        db.refresh(new_menu_item)
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+
+    return new_menu_item
+
+def read_all(db: Session):
+    try:
+        result = db.query(model.Order).all()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return
+
+def read_one(db: Session, item_id):
+    try:
+        item = db.query(model.Order).filter(model.MenuItem.id == item_id).first()
+        if not item:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return item
+
+def update(db: Session, item_id: int, request: MenuItemsUpdate):
+    try:
+        item = db.query(model.MenuItem).filter(model.MenuItem.id == item_id)
+        db_item = item.first()
+        if not db_item:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        update_data = request.dict(exclude_unset=True)
+        item.update(update_data, synchronize_session=False)
+        db.commit()
+        db.refresh(item)
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return db_item
+
+def delete(db: Session, item_id):
+    try:
+        item = db.query(model.MenuItem).filter(model.MenuItem.id == item_id)
+        db_item = item.first()
+        if not db_item:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        item.delete(synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
---
+++ FinalProject/api/controllers/resources.py
@@ -0,0 +1,67 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response
+from ..models import resources as model
+from ..schemas.resource import ResourceCreate, ResourceUpdate
+from sqlalchemy.exc import SQLAlchemyError
+
+def creat(db: Session, request: ResourceCreate):
+    new_resource = model.Resource(
+        item = request.Item,
+        amount = request.amount,
+        unit = request.unit
+    )
+    try:
+        db.add(new_resource)
+        db.commit()
+        db.refresh(new_resource)
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+
+    return new_resource
+
+def read_all(db: Session):
+    try:
+        result = db.query(model.Resource).all()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return result
+
+def read_one(db: Session, item_id):
+    try:
+        item = db.query(model.Resource).filter(model.Resource.id == item_id).first()
+        if not item:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return item
+
+def update(db: Session, item_id: int, request: ResourceUpdate):
+    try:
+        item = db.query(model.Resource).filter(model.Resource.id == item_id)
+        db_item = item.first()
+        if not db_item:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        update_data = request.dict(exclude_unset=True)
+        item.update(update_data, synchronize_session=False)
+        db.commit()
+        db.refresh(item)
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return db_item
+
+def delete(db: Session, item_id):
+    try:
+        item = db.query(model.Resource).filter(model.Resource.id == item_id)
+        db_item = item.first()
+        if not db_item:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        item.delete(synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
