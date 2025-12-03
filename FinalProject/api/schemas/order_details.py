f59f02ed5b45ed2dfb160323b8f362f83c78f337
devin <166536125+Devinthedude515@users.noreply.github.com>
2025-12-02 19:20:21 -0500

    Fixed orders and order details relationship

--- FinalProject/api/schemas/order_details.py
+++ FinalProject/api/schemas/order_details.py
@@ -6,9 +6,13 @@ from .menu_items import MenuItem
 class OrderDetailBase(BaseModel):
     order_id: int
     menu_item_id: int
-    quantity: int
-    price: float
-
+    tracking_number: str
+    customer_name: str
+    item_name: str
+    order_status: str
+    order_type: str
+    amount: float
+    billing_address: Optional[str] = None
 
 class OrderDetailCreate(OrderDetailBase):
     pass
@@ -17,29 +21,15 @@ class OrderDetailCreate(OrderDetailBase):
 class OrderDetailUpdate(BaseModel):
     quantity: Optional[int] = None
     price: Optional[float] = None
-    customer_name: Optional[str] = None
-    item_name: Optional[str] = None
     order_status: Optional[str] = None
-    order_type: Optional[str] = None
-    billing_address: Optional[str] = None
-    amount: Optional[float] = None
-
 
 class StatusUpdate(BaseModel):
     id: int
     order_status: str
 
-
 class OrderDetail(OrderDetailBase):
-    id: Optional[int] = None
-    customer_name: Optional[str] = None
-    item_name: Optional[str] = None
-    order_status: Optional[str] = None
-    order_type: Optional[str] = None
-    tracking_number: Optional[str] = None
-    billing_address: Optional[str] = None
-    amount: Optional[float] = None
-    menu_items: Optional[MenuItem] = None
+    id: int
+    menu_item: Optional[MenuItem] = None  # FIX name to match model
 
     class ConfigDict:
         from_attributes = True
